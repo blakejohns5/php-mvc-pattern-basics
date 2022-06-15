4ee77d94f427c7ebe6f5dbe3d47b4fb72c5a364d
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-06-15 15:21:55 +0200

    initial commit: baseConstants.php, constants.php, db.php

---
+++ config/baseConstants.php
@@ -0,0 +1,24 @@
+<?php
+
+
+// provide constants for path, by defining BASE_PATH and BASE_URL
+
+// store path from full directory
+$documentRoot = getcwd();
+define("BASE_PATH", $documentRoot);
+
+// store path from local directory to filename
+$uri = $_SERVER['REQUEST_URI'];
+
+if (isset($uri) && $uri !== null) {
+  $uri = substr($uri, 1);     // remove first '/', store rel path to filename
+  $uri = explode('/', $uri);      // separate $uri by '/', into array
+  // $_SERVER['HTTP_HOST'], sim not same as SERVER_NAME > localhost;
+  // $uri[0] returns root folder without filename
+  $uri = "http://$_SERVER[HTTP_HOST]" . "/" . $uri[0];
+} else {
+  $uri = null;
+};
+
+define("BASE_URL", $uri);
+
---
+++ config/db.php
@@ -0,0 +1,20 @@
+<?php
+
+
+include_once 'baseConstants.php';
+
+// controllers
+DEFINE ("CONTROLLERS", BASE_PATH . '/controllers/');
+
+// views
+DEFINE ("VIEWS", BASE_PATH . '/views/');
+
+// models
+DEFINE ("MODELS", BASE_PATH . '/models/');
+
+// resources
+DEFINE ("RESOURCES", BASE_PATH . '/resources/');
+
+
+echo CONTROLLERS . '<br><br>' . VIEWS . '<br><br>' . MODELS . '<br><br>' . RESOURCES;
+
